ec4e2a54370b625de8961fdc18bae500907c2f23
Axel Gard <axel.gard@protonmail.com>
2024-07-10 20:56:41 +0200

    changed time delay to 1min

--- grazing/__main__.py
+++ grazing/__main__.py
@@ -6,7 +6,8 @@ import logging
 def main():
     while True: 
         logging.warn(f"hello world {datetime.datetime.now().strftime('%H:%M:%S')}")
-        time.sleep(1)
+        time.sleep(60)
+        
 
 
 if __name__ == "__main__":
