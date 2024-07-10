87159aa445a2b08ddc3a751359a61084fb99c95b
Axel Gard <axel.gard@protonmail.com>
2024-07-10 21:07:45 +0200

    added status to install

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd "/home/axel/Programs/repositories/grazing-cira/"
-mkdir "/etc/grazing-cira"
-ln -sf ./run.sh /etc/grazing-cira/run.sh
-
 # systemd stuff 
+sudo apt-get install systemd
 sudo systemctl link "/home/axel/Programs/repositories/grazing-cira/grazing.service"
 sudo systemctl daemon-reload
 sudo systemctl enable grazing.service
 
 if [[ $* == *--start* ]]; then 
     sudo systemctl start grazing.service
+    sleep(2)
+    sudo systemctl status grazing.service
 fi
+echo "Done"
