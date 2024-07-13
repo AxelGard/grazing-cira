0105e4d5660f394c4767519155fcef43131d18c9
Axel Gard <axel.gard@protonmail.com>
2024-07-13 10:18:58 +0200

    made bars and price pipes

--- grazing/__main__.py
+++ grazing/__main__.py
@@ -1,19 +1,28 @@
-import time 
 import datetime
 import logging
 import cira 
 
-from grazing.features.bars import BarsLoad
-from datetime import datetime
+from grazing.features.bars import BarsPipe, PricePipe
+from datetime import datetime, timedelta
+from sklearn.pipeline import Pipeline
+import pandas as pd 
 
 def main():
     cira.auth.KEY_FILE = "../alpc_key.json"
     assert cira.auth.check_keys(), "the set keys dose not work"
-    start = datetime(2021, 1, 1).date()
-    end   = datetime(2023, 1, 1).date()
-    bar = BarsLoad("BTC/USD", start, end)
-    df = bar.load()
-    print(df)
+
+    start = datetime(2021, 1, 1)
+    end   = datetime.today() - timedelta(days=1) 
+    symbols = ["AAPL", "MSFT", "AMZN", "INTC", "GOOGL"] 
+
+    bars = pd.DataFrame()
+    bars_pipe = Pipeline([(f"bars_{sym}", BarsPipe(sym, start, end)) for sym in symbols])
+    bars = bars_pipe.fit_transform(bars)
+
+    prices_pipe =  Pipeline([(f"bars_{sym}", PricePipe(sym, "close")) for sym in symbols])
+    prices = prices_pipe.fit_transform(bars)
+    print(prices)
+    
 
 if __name__ == "__main__":
     main()
--- grazing/features/bars.py
+++ grazing/features/bars.py
@@ -1,3 +1,4 @@
+from operator import is_
 import pandas as pd 
 from datetime import date
 from os.path import exists
@@ -10,16 +11,21 @@ from grazing.config import DATA_STORE_PATH
 
 class BarsPipe(base.FeatruePipe):
  
-    def transform(self, X: pd.DataFrame):
-        pass
-
-
-class BarsLoad(base.FeatureLoad):
-    def __init__(self, symbol:str, start:date, end:date) -> None:
+    def __init__(self, symbol:str, start:datetime, end:datetime, add_symbol_to_col:bool=True) -> None:
+        super().__init__()
         self.symbol = symbol
-        self.start = self.closest_business_day(start).date()
-        self.end = self.closest_business_day(end).date()
+        self.start = start.date()
+        self.end = end.date()
         self.is_crypto = "/" in symbol
+        self.add_symbol_to_col = add_symbol_to_col
+
+    def transform(self, X: pd.DataFrame):
+        df = self.load()
+        df.drop(columns=["symbol"], inplace=True)
+        if self.add_symbol_to_col: 
+            for i, col_name in enumerate(df.keys()):
+                df.columns.values[i] = f"{self.symbol}_{col_name}"
+        return pd.concat([X, df], axis=1)
 
     def load(self) -> pd.DataFrame:
         global DATA_STORE_PATH
@@ -32,25 +38,39 @@ class BarsLoad(base.FeatureLoad):
 
         if exists(file_path):
             df = ast.load_historical_data(file_path)
-            print(f"{df.index[0].date()} == {self.start} and {df.index[-1].date()} == {self.end}")
             if df.index[0].date() == self.start and df.index[-1].date() == self.end:
-                print("use all saved data")
                 return df
             if df.index[0].date() != self.start: 
-                print("bad start date")
                 ast.save_historical_data(file_path, self.start, self.end)
                 df = ast.load_historical_data(file_path)
                 return df
             if df.index[-1].date() < self.end:  
-                print("bad end date")
                 start = datetime.fromisoformat(df.index[-1].isoformat()).date()
                 new_df = ast.historical_data_df(start, self.end)
                 df = pd.concat([df, new_df])
                 df.to_csv(file_path)
                 return df
 
-        print("get all new data")
         df = ast.historical_data_df(self.start, self.end)
         df.to_csv(file_path)
         return df
+
+
+class PricePipe(base.FeatruePipe):
+    
+    def __init__(self, symbol:str, column_as_price:str="close", is_symbol_in_col:bool=True) -> None:
+        super().__init__()
+        self.column_as_price = column_as_price
+        self.symbol = symbol
+        self.is_symbol_in_col = is_symbol_in_col
+
+    def transform(self, X: pd.DataFrame):
+        if not self.is_symbol_in_col: 
+            return X[self.column_as_price].to_frame()
+         
+        for col_name in X.keys():
+            if self.column_as_price in col_name: 
+                X[self.symbol] = X[col_name]
+        return X.drop(columns=[col_name for col_name in X.keys() if self.symbol in col_name and col_name != self.symbol])    
+        
 
