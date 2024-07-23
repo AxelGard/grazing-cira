8561ec6dad581caadb30425c3a83162774da77b4
Axel Gard <axel.gard@protonmail.com>
2024-07-23 10:37:07 +0200

    added typing to load strat

--- grazing/strategies/__init__.py
+++ grazing/strategies/__init__.py
@@ -1,10 +1,11 @@
 import cira
 from grazing.strategies.bollinger_bands import BollingStartegy
 import logging
+from typing import Union
 
 __strat_log = logging.getLogger(__name__)
 
-def load_startegy(strategy_name:str):
+def load_startegy(strategy_name:str) -> Union[None,cira.strategy.Strategy]:
     global avilabel_strategies, log
     if not strategy_name in avilabel_strategies.keys():
         __strat_log.critical(f"given strategy name ({strategy_name}) has not been setup")
